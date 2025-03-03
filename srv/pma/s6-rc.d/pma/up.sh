f2d94ef741e77319d84d9310bb07dfd47e67f316
n0099 <n@n0099.net>
2025-03-03 18:17:08 +0000

    * migrate from `base/{nginx,php-fpm}` to `base/s6.nginx.php-fpm` * migrate `php-fpm.Dockerfile` to `s6-rc.d/pma/up.sh` * migrate `$PHP_EXTENSIONS` in `.env` to `s6-rc.d/php-extensions/up` and install more extensions that not included in Alpine Linux package https://pkgs.alpinelinux.org/package/v3.21/community/x86_64/php83 @ srv/pma

---
+++ srv/pma/s6-rc.d/pma/up.sh
@@ -0,0 +1,10 @@
+#!/bin/ash -eux
+[ -f /var/www/html/index.php ] && exit 0
+cd /var/www/html
+apk add curl
+curl -LOJ https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.xz
+tar xf phpMyAdmin-latest-all-languages.tar.xz
+rm phpMyAdmin-latest-all-languages.tar.xz
+mv phpMyAdmin-*-all-languages/* .
+rm -r phpMyAdmin-*-all-languages
+chown -R www-data: .
