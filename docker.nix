45191965510f7dc5bd9332555a1957f1c3731f2f
n0099 <n@n0099.net>
2025-10-07 11:05:42 +0000

    * add user `n0099` into group `docker` to grant access to docker daemon @ docker.nix * create user `dockremap` with uid and gid `524288` and its id range for user namespace @ docker.nix

--- docker.nix
+++ docker.nix
@@ -1,12 +1,37 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
-{
-  # users.users.n0099.extraGroups = [ "docker" ];
-  virtualisation.docker = {
-    enable = true;
-    package = pkgs.docker_28;
-    storageDriver = "zfs";
-    autoPrune.enable = true;
-    daemon.settings.userns-remap = "default";
-  };
-}
+lib.mkMerge [
+  {
+    users.users.n0099.extraGroups = [ "docker" ];
+    virtualisation.docker = {
+      enable = true;
+      package = pkgs.docker_28;
+      storageDriver = "zfs";
+      autoPrune.enable = true;
+    };
+  }
+  {
+    # https://docs.docker.com/engine/security/userns-remap/
+    virtualisation.docker.daemon.settings.userns-remap = "default";
+    users = {
+      users.dockremap = {
+        isSystemUser = true;
+        subUidRanges = [
+          # https://systemd.io/UIDS-GIDS/
+          {
+            count = 65536;
+            startUid = 524288;
+          }
+        ];
+        subGidRanges = [
+          {
+            count = 65536;
+            startGid = 524288;
+          }
+        ];
+        group = "dockremap";
+      };
+      groups.dockremap = { };
+    };
+  }
+]
