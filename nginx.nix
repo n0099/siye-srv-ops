d3ca2b1669fac2bde43e1d2b99c79cbe55a5523f
n0099 <n@n0099.net>
2025-10-12 08:37:51 +0000

    * no longer using self-signed TLS cert from vhost `default` for vhost `z.n0099.net` @ nginx.nix

--- nginx.nix
+++ nginx.nix
@@ -1,18 +1,31 @@
-{ config, lib, ... }:
+{ lib, ... }:
 
+let
+  certByDomain =
+    domain:
+    let
+      certBasePath = "/etc/ssl/certs/${domain}";
+    in
+    {
+      forceSSL = true;
+      sslCertificate = "${certBasePath}/fullchain.pem"; # https://stackoverflow.com/questions/26191463/ssl-error0b080074x509-certificate-routinesx509-check-private-keykey-values/41154564#41154564
+      sslCertificateKey = "${certBasePath}/privkey.pem";
+    };
+in
 {
-  services.nginx.virtualHosts = {
-    "z.n0099.net" = {
-      inherit (config.services.nginx.virtualHosts.default) forceSSL sslCertificate sslCertificateKey;
-      locations = {
-        "/" = {
-          proxyPass = (import ./base/toBeFilled/lib.nix lib).readString ./toBeFilled/nginx/zulip/proxyPass;
-          extraConfig = ''
-            # https://zulip.readthedocs.io/en/9.4/production/reverse-proxies.html#nginx-configuration
-            proxy_buffering off;
-          '';
+  services.nginx = {
+    virtualHosts = {
+      "z.n0099.net" = (certByDomain "n0099.net") // {
+        locations = {
+          "/" = {
+            proxyPass = (import ./base/toBeFilled/lib.nix lib).readString ./toBeFilled/nginx/zulip/proxyPass;
+            extraConfig = ''
+              # https://zulip.readthedocs.io/en/9.4/production/reverse-proxies.html#nginx-configuration
+              proxy_buffering off;
+            '';
+          };
+          "/error/".root = "/srv/www/n0099";
         };
-        "/error/".root = "/srv/www/n0099";
       };
       extraConfig = ''
         error_page 502 /error/502_zulip.html;
