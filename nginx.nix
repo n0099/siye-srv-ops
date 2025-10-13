3fe26b34be2d6f5854d83cde3b5e2f45ba5c0bf3
n0099 <n@n0099.net>
2025-10-13 14:40:03 +0000

    * turn the flatten var `proxyPassPortsKeyByUri` into structure like `vhost[].location = port` + set values of `virtualHosts.*.locations.*.proxyPass` that based on `proxyPassPortsByUrl` @ nginx.nix

--- nginx.nix
+++ nginx.nix
@@ -1,17 +1,19 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 let
-  proxyPassPortsKeyByUri = [
-    { "simcity.moe" = 9003; }
-    { "mcbar.club" = 9004; }
-    { "n0099.net/v" = 9005; }
-    { "n0099.net/tc" = 9006; }
-    { "n0099.net/pma" = 9007; }
-    { "n0099.net/tbm/v1" = 9008; }
-    { "n0099.net/tbm/be" = 9009; }
-    { "n0099.net/tbm" = 3001; }
-    { "z.n0099.net" = 9002; }
-  ];
+  proxyPassPortsByUrl = {
+    "z.n0099.net" = [ { "/" = 9002; } ];
+    "simcity.moe" = [ { "/" = 9003; } ];
+    "mcbar.club" = [ { "/" = 9004; } ];
+    "n0099.net" = [
+      { "/v" = 9005; }
+      { "/tc" = 9006; }
+      { "/pma" = 9007; }
+      { "/tbm/v1" = 9008; }
+      { "/tbm/be" = 9009; }
+      { "/tbm" = 3001; }
+    ];
+  };
   certByDomain =
     domain:
     let
@@ -24,33 +26,66 @@ let
     };
 in
 {
-  n0099.nginx.baseUrls = lib.concatMap (pair: lib.attrNames pair) proxyPassPortsKeyByUri;
+  n0099.nginx.baseUrls = lib.flatten (
+    lib.mapAttrsToList (
+      domain: baseUrlsKeyByPort:
+      lib.concatMap (
+        baseUrlKeyByPort: lib.map (baseUrl: "${domain}${baseUrl}") (lib.attrNames baseUrlKeyByPort)
+      ) baseUrlsKeyByPort
+    ) proxyPassPortsByUrl
+  );
   services.nginx = {
     appendHttpConfig = ''
       map $host$uri $proxy_pass_port {
         ${lib.concatStringsSep "\n" (
-          lib.concatMap (lib.mapAttrsToList (
-            uri: port: "~^${lib.escapeRegex uri}/ ${builtins.toString port};"
-          )) proxyPassPortsKeyByUri
+          lib.flatten (
+            lib.mapAttrsToList (
+              domain: baseUrlsKeyByPort:
+              lib.concatMap (
+                baseUrlKeyByPort:
+                lib.mapAttrsToList (
+                  baseUrl: port:
+                  let
+                    hostUrlPattern = lib.escapeRegex "${domain}${baseUrl}";
+                    traillingSlash = if baseUrl == "/" then "" else "/";
+                  in
+                  "~^${hostUrlPattern}${traillingSlash} ${builtins.toString port};"
+                ) baseUrlKeyByPort
+              ) baseUrlsKeyByPort
+            ) proxyPassPortsByUrl
+          )
         )}
       }
     '';
-    virtualHosts = {
-      "z.n0099.net" = (certByDomain "n0099.net") // {
-        locations = {
-          "/" = {
-            proxyPass = "http://127.0.0.1:$proxy_pass_port";
-            extraConfig = ''
-              # https://zulip.readthedocs.io/en/9.4/production/reverse-proxies.html#nginx-configuration
-              proxy_buffering off;
-            '';
+    virtualHosts = lib.mkMerge [
+      (lib.genAttrs [
+        "mcbar.club"
+        "simcity.moe"
+        "n0099.net"
+      ] (domain: certByDomain domain))
+      (lib.mapAttrs (_: baseUrlsKeyByPort: {
+        locations = lib.mkMerge (
+          lib.map (lib.mapAttrs (
+            _: _: { proxyPass = "http://127.0.0.1:$proxy_pass_port"; }
+          )) baseUrlsKeyByPort
+        );
+      }) proxyPassPortsByUrl)
+      {
+        "z.n0099.net" = (certByDomain "n0099.net") // {
+          locations = {
+            "/" = {
+              extraConfig = ''
+                # https://zulip.readthedocs.io/en/9.4/production/reverse-proxies.html#nginx-configuration
+                proxy_buffering off;
+              '';
+            };
+            "/error/".root = "/srv/www/n0099";
           };
-          "/error/".root = "/srv/www/n0099";
+          extraConfig = ''
+            error_page 502 /error/502_zulip.html;
+          '';
         };
-        extraConfig = ''
-          error_page 502 /error/502_zulip.html;
-        '';
-      };
-    };
+      }
+    ];
   };
 }
