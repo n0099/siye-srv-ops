fde66e34f2a0cfa917f34844fc4f2cc23ff202dc
n0099 <n@n0099.net>
2025-10-14 19:10:29 +0000

    * fix missing trailing slash in the location `/posts` will match anything with that prefix * fix missing leading slash for locations with certain allowed path that won't redirect to the root * replace the host for proxy passing from hardcoded `127.0.0.1` to `localhost` as https://nginx.org/r/resolver have been set in commit ec7a33187f5c97ea6baf9cfbddd4b2e2d9bb69df of submodule `base` @ nginx.nix
    $ git submodule update --remote

--- nginx.nix
+++ nginx.nix
@@ -45,7 +45,7 @@ in
         (lib.mapAttrs (_: baseUrlsKeyByPort: {
           locations = lib.mkMerge (
             lib.map (lib.mapAttrs (
-              _: port: { proxyPass = "http://127.0.0.1:${builtins.toString port}"; }
+              _: port: { proxyPass = "http://localhost:${builtins.toString port}"; }
             )) baseUrlsKeyByPort
           );
         }) proxyPassPortsByUrl)
@@ -66,7 +66,6 @@ in
         {
           "n0099.net" = lib.mkMerge [
             {
-              locations."/error" = { };
               extraConfig = ''
                 error_page 401 /error/401.html;
                 error_page 403 /error/403.html;
@@ -77,14 +76,20 @@ in
             }
             {
               root = "/srv/www/n0099";
-              locations = lib.genAttrs [ "favicon.ico" "robots.txt" "BingSiteAuth.xml" ] (_: { });
+              locations =
+                (lib.genAttrs (lib.map (url: "/${url}") [
+                  "error"
+                  "favicon.ico"
+                  "robots.txt"
+                  "BingSiteAuth.xml"
+                ]) (_: { }))
+                // {
+                  "= /".tryFiles = "/index.html =404";
+                  "/".return = "302 https://n0099.net";
+                };
             }
             {
               locations = lib.mkMerge [
-                {
-                  "= /".tryFiles = "/index.html =404";
-                  "/".return = "302 https://n0099.net";
-                }
                 {
                   "~ '/tbm/imgsrc/([0-9a-f]{40}|[0-9a-f]{24})'" = {
                     # https://github.com/lumina37/aiotieba/pull/63#issuecomment-2447263162
@@ -99,7 +104,7 @@ in
                     '';
                   };
                   "~ ^/tbm/tbm/([^\\r\\n]*)".return = "301 /tbm/$1"; # temp fix for google seo due to https://github.com/harlan-zw/nuxt-site-config/issues/32
-                  "/posts".return = "301 /tbm$request_uri"; # temp fix for google trying to crawl https://n0099.net/posts/*
+                  "/posts/".return = "301 /tbm$request_uri"; # temp fix for google trying to crawl https://n0099.net/posts/*
                 }
                 {
                   "/mc/".return = "302 https://mc.n0099.net:44444";
