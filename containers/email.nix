df45faf95861bb757a5573a803d4ffb6235e5e1f
n0099 <n@n0099.net>
2025-11-01 10:03:18 +0000

    * fix the value of `{pass,user}db.args` in dovecot config can only be absolute path * fix not building dovecot with mysql driver @ container/email.nix

--- containers/email.nix
+++ containers/email.nix
@@ -266,19 +266,27 @@ in
             bindMounts."${dbConnect}".isReadOnly = true;
             config =
               let
-                sqlArgsFile = "dovecot-sql.conf.ext";
+                sqlArgsFilePath = "dovecot/dovecot-sql.conf.ext";
               in
               {
+                nixpkgs.overlays = [
+                  (self: super: {
+                    # https://github.com/NixOS/nixpkgs/blob/78e34d1667d32d8a0ffc3eba4591ff256e80576e/pkgs/by-name/do/dovecot/package.nix#L37
+                    # https://github.com/NixOS/nixpkgs/pull/14898
+                    dovecot = super.dovecot.override { withMySQL = true; };
+                  })
+                ];
                 services.dovecot2.extraConfig = ''
                   passdb {
                     driver = sql
-                    args = ${sqlArgsFile}
+                    args = /etc/${sqlArgsFilePath}
                   }
                   userdb {
                     driver = sql
-                    args = ${sqlArgsFile}
-                  }'';
-                environment.etc."dovecot/${sqlArgsFile}".text = ''
+                    args = /etc/${sqlArgsFilePath}
+                  }
+                '';
+                environment.etc.${sqlArgsFilePath}.text = ''
                   !include ${dbConnect}
                   driver = mysql
                   default_pass_scheme = ARGON2ID
