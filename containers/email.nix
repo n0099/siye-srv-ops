223271132cef4430109807c3a72ce2cfb66b881d
n0099 <n@n0099.net>
2025-11-12 16:01:17 +0000

    * mark systemd unit of container as part of `mysql` as runs after it to update its socket file that bind mounted in container @ container/email.nix

--- containers/email.nix
+++ containers/email.nix
@@ -231,96 +231,6 @@ lib.mkMerge [
             };
           };
       }
-      (
-        let
-          mysqlSocket = "/run/mysqld/mysqld.sock";
-        in
-        lib.mkMerge [
-          { bindMounts.${mysqlSocket}.isReadOnly = true; }
-          (
-            let
-              hostPrivateKey = "/etc/ssh/ssh_host_ed25519_key";
-              secretName = "roundcube.db.password";
-            in
-            {
-              bindMounts."${hostPrivateKey}".isReadOnly = true;
-              config =
-                { ... }@container:
-
-                {
-                  # https://wiki.nixos.org/wiki/Agenix#Access_secrets_inside_a_container
-                  imports = [ inputs.agenix.nixosModules.default ];
-                  age = {
-                    identityPaths = [ hostPrivateKey ];
-                    secrets.${secretName} = {
-                      file = ../secrets/roundcube.db.password.age;
-                      symlink = false;
-                      owner = "nginx";
-                    };
-                  };
-                  services.roundcube = {
-                    database = {
-                      host = "unix(${mysqlSocket})";
-                      passwordFile = container.config.age.secrets.${secretName}.path;
-                      dbname = "email";
-                      username = "email";
-                    };
-                    extraConfig = ''
-                      $config['db_dsnw'] = preg_replace('#^pgsql://#', 'mysql://', $config['db_dsnw']);
-                      $config['db_prefix'] = 'roundcube_';
-                    '';
-                  };
-                };
-            }
-          )
-          (
-            let
-              dbConnect = config.age.secrets."dovecot.db.connect".path;
-            in
-            {
-              bindMounts."${dbConnect}".isReadOnly = true;
-              config =
-                let
-                  sqlArgsFilePath = "dovecot/dovecot-sql.conf.ext";
-                in
-                {
-                  nixpkgs.overlays = [
-                    (self: super: {
-                      # https://github.com/NixOS/nixpkgs/blob/78e34d1667d32d8a0ffc3eba4591ff256e80576e/pkgs/by-name/do/dovecot/package.nix#L37
-                      # https://github.com/NixOS/nixpkgs/pull/14898
-                      dovecot = super.dovecot.override { withMySQL = true; };
-                    })
-                  ];
-                  services.dovecot2.extraConfig = ''
-                    passdb {
-                      driver = sql
-                      args = /etc/${sqlArgsFilePath}
-                    }
-                    userdb {
-                      driver = sql
-                      args = /etc/${sqlArgsFilePath}
-                    }
-                  '';
-                  environment.etc.${sqlArgsFilePath}.text = ''
-                    !include ${dbConnect}
-                    driver = mysql
-                    default_pass_scheme = ARGON2ID
-                    # https://doc.dovecot.org/2.3/admin_manual/system_users_used_by_dovecot/#uids
-                    # https://systemd.io/UIDS-GIDS/
-                    # https://man.archlinux.org/man/login.defs.5
-                    user_query = \
-                      SELECT username, uid, gid \
-                      FROM dovecot_users WHERE username = '%n' AND domain = '%d'
-                    password_query = \
-                      SELECT username, domain, password \
-                      FROM dovecot_users WHERE username = '%n' AND domain = '%d'
-                    iterate_query = SELECT username AS user FROM dovecot_users
-                  '';
-                };
-            }
-          )
-        ]
-      )
       {
         config =
           { pkgs, ... }:
@@ -441,5 +351,104 @@ lib.mkMerge [
           ];
       }
     ];
+  }
+  {
+    systemd.services."container@email" =
+      let
+        mysql = [ "mysql.service" ];
+      in
+      {
+        after = mysql;
+        partOf = mysql; # https://serverfault.com/questions/812492/systemd-automatically-start-restart-specific-systemd-service-after-another-ser
+      };
+    containers.email =
+      let
+        mysqlSocket = "/run/mysqld/mysqld.sock";
+      in
+      lib.mkMerge [
+        { bindMounts.${mysqlSocket}.isReadOnly = true; }
+        (
+          let
+            hostPrivateKey = "/etc/ssh/ssh_host_ed25519_key";
+            secretName = "roundcube.db.password";
+          in
+          {
+            bindMounts."${hostPrivateKey}".isReadOnly = true;
+            config =
+              { ... }@container:
+
+              {
+                # https://wiki.nixos.org/wiki/Agenix#Access_secrets_inside_a_container
+                imports = [ inputs.agenix.nixosModules.default ];
+                age = {
+                  identityPaths = [ hostPrivateKey ];
+                  secrets.${secretName} = {
+                    file = ../secrets/roundcube.db.password.age;
+                    symlink = false;
+                    owner = "nginx";
+                  };
+                };
+                services.roundcube = {
+                  database = {
+                    host = "unix(${mysqlSocket})";
+                    passwordFile = container.config.age.secrets.${secretName}.path;
+                    dbname = "email";
+                    username = "email";
+                  };
+                  extraConfig = ''
+                    $config['db_dsnw'] = preg_replace('#^pgsql://#', 'mysql://', $config['db_dsnw']);
+                    $config['db_prefix'] = 'roundcube_';
+                  '';
+                };
+              };
+          }
+        )
+        (
+          let
+            dbConnect = config.age.secrets."dovecot.db.connect".path;
+          in
+          {
+            bindMounts."${dbConnect}".isReadOnly = true;
+            config =
+              let
+                sqlArgsFilePath = "dovecot/dovecot-sql.conf.ext";
+              in
+              {
+                nixpkgs.overlays = [
+                  (self: super: {
+                    # https://github.com/NixOS/nixpkgs/blob/78e34d1667d32d8a0ffc3eba4591ff256e80576e/pkgs/by-name/do/dovecot/package.nix#L37
+                    # https://github.com/NixOS/nixpkgs/pull/14898
+                    dovecot = super.dovecot.override { withMySQL = true; };
+                  })
+                ];
+                services.dovecot2.extraConfig = ''
+                  passdb {
+                    driver = sql
+                    args = /etc/${sqlArgsFilePath}
+                  }
+                  userdb {
+                    driver = sql
+                    args = /etc/${sqlArgsFilePath}
+                  }
+                '';
+                environment.etc.${sqlArgsFilePath}.text = ''
+                  !include ${dbConnect}
+                  driver = mysql
+                  default_pass_scheme = ARGON2ID
+                  # https://doc.dovecot.org/2.3/admin_manual/system_users_used_by_dovecot/#uids
+                  # https://systemd.io/UIDS-GIDS/
+                  # https://man.archlinux.org/man/login.defs.5
+                  user_query = \
+                    SELECT username, uid, gid \
+                    FROM dovecot_users WHERE username = '%n' AND domain = '%d'
+                  password_query = \
+                    SELECT username, domain, password \
+                    FROM dovecot_users WHERE username = '%n' AND domain = '%d'
+                  iterate_query = SELECT username AS user FROM dovecot_users
+                '';
+              };
+          }
+        )
+      ];
   }
 ]
