3dd0b0d00940de5309ed2c1e5e19f71b0aa93036
n0099 <git@n0099.com>
2026-01-02 13:30:18 +0000

    $ mv containers/email . * fix relative path @ email/mysql.nix @ modules
    $ git submodule update --remote

--- modules/email/mysql.nix
+++ modules/email/mysql.nix
@@ -39,7 +39,7 @@
                     age = {
                       identityPaths = [ hostPrivateKey ];
                       secrets.${secretName} = {
-                        file = ../../../secrets/roundcube.db.password.age;
+                        file = ../../secrets/roundcube.db.password.age;
                         symlink = false;
                         owner = "nginx";
                       };
