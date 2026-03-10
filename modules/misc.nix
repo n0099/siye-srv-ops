016ff59baefa39e58472227d004a525dc2e8432d
n0099 <git@n0099.com>
2026-03-10 15:58:09 +0000

    * adopt changes in commit 590cd73d3e2f6289204a8bdeabd8dc5b05069321 of submodule `base` @ modules/misc.nix $ nix flake update $ git submodule update --remote

--- modules/misc.nix
+++ modules/misc.nix
@@ -10,12 +10,11 @@
         };
       }
       {
-        n0099.cachyos.baseKernel = pkgs.linuxPackages_6_18.kernel;
-        boot.zfs.package = pkgs.zfs_2_4;
-      }
-      {
         networking.hostId = "c7635afd";
-        boot.zfs.devNodes = "/dev/disk/by-partuuid"; # https://discourse.nixos.org/t/21-05-zfs-root-install-cant-import-pool-on-boot/13652/7
+        boot.zfs = {
+          package = pkgs.zfs_2_4;
+          devNodes = "/dev/disk/by-partuuid"; # https://discourse.nixos.org/t/21-05-zfs-root-install-cant-import-pool-on-boot/13652/7
+        };
       }
       {
         n0099.stdenv.enable = true;
