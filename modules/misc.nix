b995e0f64197a6ef6f784ab8a2d53ab8ffbff9e0
n0099 <git@n0099.com>
2026-04-25 12:54:11 +0000

    * enable option `n0099.sanoid` to adopt change in https://github.com/n0099/nixos/commit/fed66e793ed9cdf1fbc8b779614535ccdbcbaaf2 @ modules/misc.nix $ git submodule update --remote

--- modules/misc.nix
+++ modules/misc.nix
@@ -10,11 +10,12 @@
         };
       }
       {
-        networking.hostId = "c7635afd";
         boot.zfs = {
           package = pkgs.zfs_2_4;
           devNodes = "/dev/disk/by-partuuid"; # https://discourse.nixos.org/t/21-05-zfs-root-install-cant-import-pool-on-boot/13652/7
         };
+        networking.hostId = "c7635afd";
+        n0099.sanoid.enable = true;
       }
       {
         n0099.stdenv.enable = true;
