8dba0d2eca2195bc893da92daf80c41994fba0f7
n0099 <git@n0099.com>
2026-06-06 22:53:25 +0000

    * fix cannot build `libtpms` which is a dependency of `systemd` with `-march=znver5`: https://github.com/NixOS/nixpkgs/issues/528643 @ modules/march.nix

--- modules/march.nix
+++ modules/march.nix
@@ -7,5 +7,13 @@
       arch = "znver5";
     };
     nix.gc.automatic = false |> lib.mkForce;
+    nixpkgs.overlays = [
+      (final: prev: {
+        libtpms = prev.libtpms.overrideAttrs (prev: {
+          # https://github.com/NixOS/nixpkgs/issues/528643
+          configureFlags = (prev.configureFlags or [ ]) ++ [ "CFLAGS=-march=x86-64-v3" ];
+        });
+      })
+    ];
   };
 }
