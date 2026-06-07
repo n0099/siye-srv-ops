f08935e4652bc0a4e83857e53716e0b3828e0c35
n0099 <git@n0099.com>
2026-06-07 13:21:55 +0000

    * fix building dovecot 2.3 with mysql: https://github.com/NixOS/nixpkgs/pull/529130 @ mysql.nix * replace nixpkgs overlay for `dovecot` that now defaults to 2.4 since https://github.com/NixOS/nixpkgs/pull/500271/changes/b5aeef311689873efc7b4cfedb3ae4847cdb9e20 in `dovecot/_passdb.nix` with overriding the 2.3 package to its service in `mysql.nix` @ modules/email

--- modules/email/dovecot/_passdb.nix
+++ modules/email/dovecot/_passdb.nix
@@ -19,16 +19,5 @@ let
 in
 {
   inherit dbConnect genArgsFilePath genDovecotPassDB;
-  containerConfig = {
-    bindMounts."${dbConnect}".isReadOnly = true;
-    config = {
-      nixpkgs.overlays = [
-        (self: super: {
-          # https://github.com/NixOS/nixpkgs/blob/78e34d1667d32d8a0ffc3eba4591ff256e80576e/pkgs/by-name/do/dovecot/package.nix#L37
-          # https://github.com/NixOS/nixpkgs/pull/14898
-          dovecot = super.dovecot.override { withMySQL = true; };
-        })
-      ];
-    };
-  };
+  containerConfig.bindMounts."${dbConnect}".isReadOnly = true;
 }
--- modules/email/mysql.nix
+++ modules/email/mysql.nix
@@ -76,27 +76,45 @@
           [
             containerConfig
             {
-              config = lib.mkMerge [
-                (genDovecotPassDB lmtpArgsFilePath)
-                {
-                  services.dovecot2.settings.userdb = {
-                    driver = "sql";
-                    args = "/etc/${lmtpArgsFilePath}";
-                  };
-                  environment.etc.${lmtpArgsFilePath}.text = /* sql */ ''
-                    # https://doc.dovecot.org/2.3/admin_manual/system_users_used_by_dovecot/#uids
-                    # https://systemd.io/UIDS-GIDS/
-                    # https://man.archlinux.org/man/login.defs.5
-                    user_query = \
-                      SELECT username, uid, gid \
-                      FROM dovecot_passdb_lmtp WHERE username = '%n' AND domain = '%d'
-                    password_query = \
-                      SELECT username, domain, password \
-                      FROM dovecot_passdb_lmtp WHERE username = '%n' AND domain = '%d'
-                    iterate_query = SELECT username AS user FROM dovecot_passdb_lmtp
-                  '';
-                }
-              ];
+              config =
+                { pkgs, ... }:
+
+                lib.mkMerge [
+                  (genDovecotPassDB lmtpArgsFilePath)
+                  {
+                    services.dovecot2.package =
+                      (pkgs.dovecot_2_3.override {
+                        # https://github.com/NixOS/nixpkgs/blob/6b316287bae2ee04c9b93c8c858d930fd07d7338/pkgs/by-name/do/dovecot/generic.nix#L48-L52
+                        withMySQL = true;
+                        withSQLite = false;
+                      }).overrideAttrs
+                        (prev: {
+                          configureFlags = (prev.configureFlags or [ ]) ++ [
+                            # https://github.com/NixOS/nixpkgs/pull/529130
+                            "CFLAGS=-I${pkgs.libmysqlclient.dev}/include/mysql"
+                            "LDFLAGS=-L${pkgs.libmysqlclient}/lib/mysql"
+                          ];
+                        });
+                  }
+                  {
+                    services.dovecot2.settings.userdb = {
+                      driver = "sql";
+                      args = "/etc/${lmtpArgsFilePath}";
+                    };
+                    environment.etc.${lmtpArgsFilePath}.text = /* sql */ ''
+                      # https://doc.dovecot.org/2.3/admin_manual/system_users_used_by_dovecot/#uids
+                      # https://systemd.io/UIDS-GIDS/
+                      # https://man.archlinux.org/man/login.defs.5
+                      user_query = \
+                        SELECT username, uid, gid \
+                        FROM dovecot_passdb_lmtp WHERE username = '%n' AND domain = '%d'
+                      password_query = \
+                        SELECT username, domain, password \
+                        FROM dovecot_passdb_lmtp WHERE username = '%n' AND domain = '%d'
+                      iterate_query = SELECT username AS user FROM dovecot_passdb_lmtp
+                    '';
+                  }
+                ];
             }
           ]
         )
