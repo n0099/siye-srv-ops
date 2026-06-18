8b8446abfcd57f85c6817ecf5ccce1fba4d2af73
n0099 <git@n0099.com>
2026-06-18 21:34:05 +0000

    * rename unused parameter `final: prev:` of function of nixpkgs overlay with `_` like https://github.com/n0099/nixos/commit/a324b940f4225709e3c9cecdefdf6737e0cf3247 of submodule `base` @ modules/march.nix

--- modules/march.nix
+++ modules/march.nix
@@ -8,7 +8,7 @@
     };
     nix.gc.automatic = false |> lib.mkForce;
     nixpkgs.overlays = [
-      (final: prev: {
+      (_: prev: {
         libtpms = prev.libtpms.overrideAttrs (prev: {
           # https://github.com/NixOS/nixpkgs/issues/528643
           configureFlags = (prev.configureFlags or [ ]) ++ [ "CFLAGS=-march=x86-64-v3" ];
