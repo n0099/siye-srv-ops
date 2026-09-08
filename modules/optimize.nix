c22df44dfdc16e248fda23f3acc2bc54b0694770
n0099 <git@n0099.com>
2026-09-08 03:40:33 +0000

    * disable flaky tests when building NodeJS: https://github.com/NixOS/nixpkgs/issues/548402 @ modules/optimize.nix

--- modules/optimize.nix
+++ modules/optimize.nix
@@ -8,5 +8,34 @@
       python = true;
     };
     nix.gc.automatic = false |> lib.mkForce;
+    nixpkgs.overlays = [
+      (
+        _: prev:
+        let # https://github.com/NixOS/nixpkgs/issues/548402
+          override =
+            pkg:
+            prev.${pkg}.overrideAttrs (prev: {
+              checkFlags = lib.map (
+                flag:
+                if lib.hasPrefix "CI_SKIP_TESTS=" flag then
+                  "${flag},"
+                  + lib.concatStringsSep "," [
+                    "test-tls-over-http-tunnel"
+                    "test-http-agent-keepalive"
+                    "test-https-proxy-request-invalid-char-in-url"
+                  ]
+                else
+                  flag
+              ) (prev.checkFlags or [ ]);
+            });
+        in
+        {
+          nodejs-slim = override "nodejs-slim";
+          nodejs_24 = prev.nodejs_24.override {
+            nodejs-slim = override "nodejs-slim";
+          };
+        }
+      )
+    ];
   };
 }
