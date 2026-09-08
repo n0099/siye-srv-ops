98940b772ceef5915d0d351457f0e488e05e7a8f
n0099 <git@n0099.com>
2026-09-08 02:37:14 +0000

    - no longer workaround that cannot build `libtpms` with `-march=znver5`: https://github.com/NixOS/nixpkgs/issues/528643, as its upstream fix https://github.com/NixOS/nixpkgs/pull/530463 already reachs the branch `nixos-26.05`, revert 8dba0d2eca2195bc893da92daf80c41994fba0f7 @ modules/optimize.nix

--- modules/optimize.nix
+++ modules/optimize.nix
@@ -8,13 +8,5 @@
       python = true;
     };
     nix.gc.automatic = false |> lib.mkForce;
-    nixpkgs.overlays = [
-      (_: prev: {
-        libtpms = prev.libtpms.overrideAttrs (prev: {
-          # https://github.com/NixOS/nixpkgs/issues/528643
-          configureFlags = (prev.configureFlags or [ ]) ++ [ "CFLAGS=-march=x86-64-v3" ];
-        });
-      })
-    ];
   };
 }
